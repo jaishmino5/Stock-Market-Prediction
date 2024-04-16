8703b0c552ca5455f958d83b7abaeaa2b80b8402
jaishmino5 <105439213+jaishmino5@users.noreply.github.com>
2024-04-16 23:51:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model(r'C:\Users\sonia\Stock Predictions Model.keras')
+model = load_model(r'Stock Predictions Model.keras')
 
 
 
